17667426bcc1bcdd1deef5d93d1d4e1113bd077a
Dheenu-Kasinathan <dk3250@nyu.edu>
2025-01-09 20:20:22 -0600

    updated the colors

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -35,13 +35,13 @@ return {
 		local hooks = require("ibl.hooks")
 		-- Create the highlight groups in the highlight setup hook
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#F2B6B8", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F6E3AE", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#A3D8F4", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#F2C6A0", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#C8E6C9", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#E0BBE4", blend = 80 })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#B2EBF2", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#604A4A", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#5C5742", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#4A5A60", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#605242", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#4A604A", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#594A60", blend = 80 })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#4A6060", blend = 80 })
 		end)
 
 		-- Pass the options to the plugin setup
